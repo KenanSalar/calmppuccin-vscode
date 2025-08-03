418fb13736399e12f79cb7da20fb9e2557dd2c18
KenanSalar <108432110+KenanSalar@users.noreply.github.com>
2025-08-03 12:45:49 +0200

    finished centralizing macchiato, frappe and started to centralize latte. adjusted build.js to replace every color from the template with the corresponding palette

---
+++ src/palettes/mocha.js
@@ -0,0 +1,25 @@
+module.exports = {
+  themeName: "Calmppuccin Mocha",
+  themeType: "dark",
+  // Accent colors
+  rosewater: "#e2cfcb",
+  flamingo: "#e9c5c5",
+  pink: "#e4b5d7",
+  mauve: "#ba9ae2",
+  red: "#e4829e",
+  maroon: "#d8939f",
+  peach: "#e2a37b",
+  yellow: "#e7d1a2",
+  green: "#99cf94",
+  teal: "#8cd3c7",
+  sky: "#80ccda",
+  sapphire: "#70b7d8",
+  blue: "#7fa6e6",
+  lavender: "#a6afe9",
+  // UI colors
+  base: "#11111a",
+  mantle: "#171724",
+  crust: "#1d1d2c",
+  uiText: "#aeb6cf",
+  // Syntax colors
+};
---
+++ src/palettes/oledppuccin.js
@@ -0,0 +1,25 @@
+module.exports = {
+  themeName: "Calmppuccin Oledppuccin",
+  themeType: "dark",
+  // Accent colors
+  rosewater: "#b3a4a2",
+  flamingo: "#b99d9d",
+  pink: "#b491aa",
+  mauve: "#987eb8",
+  red: "#b66a7f",
+  maroon: "#b47b85",
+  peach: "#b38163",
+  yellow: "#a89978",
+  green: "#80a57b",
+  teal: "#6c9e96",
+  sky: "#669ea8",
+  sapphire: "#5b8ea8",
+  blue: "#6d8fc5",
+  lavender: "#8c92c2",
+  // UI colors
+  base: "#000000",
+  mantle: "#07070c",
+  crust: "#0b0b11",
+  uiText: "#8c92a7",
+  // Syntax colors
+};
