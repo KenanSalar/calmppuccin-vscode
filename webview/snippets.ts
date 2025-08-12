4c65a6e56b35917a531db5cdd52a02f9fb8b7047
KenanSalar <108432110+KenanSalar@users.noreply.github.com>
2025-08-12 20:04:54 +0200

    feat: refactor color picker to use a pop-up instead of a modal, update styles and related functionality

--- webview/snippets.ts
+++ webview/snippets.ts
@@ -208,7 +208,55 @@ export const codeSnippets: CodeSnippets = {
 <span data-token="punctuation">}</span>
 `,
   css: `
+<span data-token="annotation">@charset</span> <span data-token="string">"UTF-8"</span><span data-token="punctuation">;</span>
+<span data-token="annotation">@import</span> <span data-token="functionAndMethod">url</span><span data-token="punctuation">(</span><span data-token="string">'https://fonts.googleapis.com/css?family=Open+Sans'</span><span data-token="punctuation">)</span><span data-token="punctuation">;</span>
 
+<span data-token="comment">/* This snippet demonstrates various CSS features to test theme highlighting.
+  Tokens like 'delegate', 'event', 'module', etc., have no CSS equivalent.
+*/</span>
+<span data-token="keyword">@namespace</span> <span data-token="namespace">svg</span> <span data-token="functionAndMethod">url</span><span data-token="punctuation">(</span><span data-token="string">http://www.w3.org/2000/svg</span><span data-token="punctuation">)</span><span data-token="punctuation">;</span>
+
+<span data-token="comment">-- CSS Custom Properties (Variables) --</span>
+<span data-token="class">:root</span> <span data-token="punctuation">{</span>
+  <span data-token="variable">--main-bg-color</span><span data-token="punctuation">:</span> <span data-token="constant">#0d1117</span><span data-token="punctuation">;</span>
+  <span data-token="variable">--main-text-color</span><span data-token="punctuation">:</span> <span data-token="functionAndMethod">rgb</span><span data-token="punctuation">(</span><span data-token="number">220</span><span data-token="punctuation">,</span> <span data-token="number">200</span><span data-token="punctuation">,</span> <span data-token="number">180</span><span data-token="punctuation">)</span><span data-token="punctuation">;</span>
+<span data-token="punctuation">}</span>
+
+<span data-token="comment">/* Universal selector with a property */</span>
+<span data-token="operator">*</span> <span data-token="punctuation">{</span>
+  <span data-token="property">box-sizing</span><span data-token="punctuation">:</span> <span data-token="keyword">border-box</span><span data-token="punctuation">;</span>
+<span data-token="punctuation">}</span>
+
+<span data-token="comment">/* ID selector (represents a unique 'struct') and attribute selector */</span>
+<span data-token="struct">#main-container</span><span data-token="attributeBracket">[</span><span data-token="fieldAndAttribute">data-layout</span><span data-token="operator">=</span><span data-token="string">"grid"</span><span data-token="attributeBracket">]</span> <span data-token="punctuation">{</span>
+  <span data-token="property">display</span><span data-token="punctuation">:</span> <span data-token="keyword">grid</span><span data-token="punctuation">;</span>
+  <span data-token="property">gap</span><span data-token="punctuation">:</span> <span data-token="number">1</span><span data-token="keyword">rem</span><span data-token="punctuation">;</span>
+<span data-token="punctuation">}</span>
+
+<span data-token="comment">/* Class selector with pseudo-class and pseudo-element */</span>
+<span data-token="class">.button</span><span data-token="punctuation">:</span><span data-token="functionAndMethod">hover</span> <span data-token="punctuation">{</span>
+  <span data-token="property">color</span><span data-token="punctuation">:</span> <span data-token="functionAndMethod">var</span><span data-token="punctuation">(</span><span data-token="variable">--main-bg-color</span><span data-token="punctuation">)</span><span data-token="punctuation">;</span>
+  <span data-token="property">border-color</span><span data-token="punctuation">:</span> <span data-token="constant">transparent</span><span data-token="punctuation">;</span>
+<span data-token="punctuation">}</span>
+
+<span data-token="class">.button</span><span data-token="punctuation">::</span><span data-token="functionAndMethod">before</span> <span data-token="punctuation">{</span>
+  <span data-token="property">content</span><span data-token="punctuation">:</span> <span data-token="string">'▶ '</span><span data-token="punctuation">;</span>
+  <span data-token="property">margin-right</span><span data-token="punctuation">:</span> <span data-token="functionAndMethod">calc</span><span data-token="punctuation">(</span><span data-token="number">10</span><span data-token="keyword">px</span> <span data-token="operator">-</span> <span data-token="number">4</span><span data-token="keyword">px</span><span data-token="punctuation">)</span><span data-token="punctuation">;</span>
+<span data-token="punctuation">}</span>
+
+<span data-token="comment">/* Keyframes Animation */</span>
+<span data-token="keyword">@keyframes</span> <span data-token="functionAndMethod">fade-in</span> <span data-token="punctuation">{</span>
+  <span data-token="keyword">from</span> <span data-token="punctuation">{</span> <span data-token="property">opacity</span><span data-token="punctuation">:</span> <span data-token="number">0</span><span data-token="punctuation">;</span> <span data-token="punctuation">}</span>
+  <span data-token="keyword">to</span> <span data-token="punctuation">{</span> <span data-token="property">opacity</span><span data-token="punctuation">:</span> <span data-token="number">1</span><span data-token="punctuation">;</span> <span data-token="punctuation">}</span>
+<span data-token="punctuation">}</span>
+
+<span data-token="comment">/* Media Query for responsive design */</span>
+<span data-token="keyword">@media</span> <span data-token="punctuation">(</span><span data-token="property">max-width</span><span data-token="punctuation">:</span> <span data-token="number">768</span><span data-token="keyword">px</span><span data-token="punctuation">)</span> <span data-token="punctuation">{</span>
+  <span data-token="type">body</span> <span data-token="punctuation">{</span>
+    <span data-token="property">font-size</span><span data-token="punctuation">:</span> <span data-token="number">14</span><span data-token="keyword">px</span> <span data-token="keyword">!important</span><span data-token="punctuation">;</span>
+    <span data-token="text">This text is a placeholder.</span>
+  <span data-token="punctuation">}</span>
+<span data-token="punctuation">}</span>
 `,
   go: `
 
